ee07ee66834a0be8672e212b69d4979eedaa9088
Yuri R Lombardi <yurirlombardi@icloud.com>
2024-06-08 18:12:19 -0300

    Uso do comando Insert Into

---
+++ MySQL/aula5/insertInto.sql
@@ -0,0 +1,7 @@
+insert into pessoas values
+(default,'Claudio','1975-4-22','M','99.0','2.15','Brasil'),
+(default,'Pedro','1999-12-3','M','87','2',default),
+(default,'Janaina','1987-11-12','F','75.4','1.66','EUA');
+
+select * from pessoas;
+
